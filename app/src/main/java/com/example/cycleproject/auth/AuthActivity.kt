3ee4a50178b687655bff8a36e86a27c8b47e3b44
bipulshre <bipulshr28@Gmail.com>
2022-05-07 09:35:55 +0545

    made login and register screen

--- app/src/main/java/com/example/cycleproject/auth/AuthActivity.kt
+++ app/src/main/java/com/example/cycleproject/auth/AuthActivity.kt
@@ -1,16 +1,14 @@
-package com.example.cycleproject
+package com.example.cycleproject.auth
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.example.cycleproject.auth.AuthActivity
+import com.example.cycleproject.R
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        installSplashScreen()
+        setContentView(R.layout.activity_auth)
     }
 }
